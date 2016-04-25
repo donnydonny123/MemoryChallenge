f370fe21a6040235614d30120744cc96d21f53a5
donnydonny123 <don.cricketsss@gmail.com>
2016-04-25 13:07:15 +0800

    XD

---
+++ app/src/main/java/com/example/tsao/memorychallenge/win_activity.java
@@ -0,0 +1,13 @@
+package com.example.tsao.memorychallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class win_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_win_activity);
+    }
+}
